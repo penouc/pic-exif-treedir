ae43dca12a499c9a26f75daef93904450b556881
pen <penouc@gmail.com>
2023-02-02 06:55:37 +0000

    feat: 完成文件夹的创建

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
-use exif::{Tag,Value, In};
-use exif::DateTime;
-use std::fs;
+use exif::{Tag, In};
+use std::{fs, env};
 use std::path::Path;
+use chrono::{NaiveDateTime};
+
+fn create_dir_if_not_exists(dir_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let path = Path::new(dir_path);
+
+    // 判断文件夹是否存在
+    if !path.exists() {
+        // 新建文件夹
+        fs::create_dir_all(path)?;
+    }
+
+    Ok(())
+}
 
 fn get_exif_time_info(dir_path: &str) -> Result<(), Box<dyn std::error::Error>> {
     let path = Path::new(dir_path);
@@ -14,6 +26,7 @@ fn get_exif_time_info(dir_path: &str) -> Result<(), Box<dyn std::error::Error>> 
         // 只处理 jpeg 和 heic 格式的文件
         if file_type == "jpeg" || file_type == "jpg" || file_type == "heic" {
             // 读取文件并解析 exif 信息
+            let file_pwd_path = path.clone();
             let file = std::fs::File::open(path)?;
             let mut bufreader = std::io::BufReader::new(&file);
             let exifreader = exif::Reader::new();
@@ -24,19 +37,39 @@ fn get_exif_time_info(dir_path: &str) -> Result<(), Box<dyn std::error::Error>> 
                     match exif_data.get_field(Tag::DateTimeOriginal, In::PRIMARY) {
                         Some(date_time_original) => {
                             
-                            println!("获取 exif 信息成功：{:?}", date_time_original.value);
-                            // match &date_time_original.value {
-                            //     Ascii(datetime_val) => {
-                            //         // let dt  = DateTime::from_ascii(dt_val)?;
-
-                            //         println!("获取 exif 信息成功：{:?}", datetime_val);
-                            //     },
-                            //     _ => {
-                            //         println!("获取 exif 信息为其他");
-                            //     }
-                            // }
-                            // let dt  = DateTime::from_ascii(date_time_original.value.Ascii)?;
-                            // println!("获取 exif 信息成功：{:?}", dt);
+                            let dt_str = date_time_original.value.display_as(Tag::DateTimeOriginal).to_string();
+                            println!("获取 exif 信息成功：{:?}", dt_str);
+                            let no_timezone = NaiveDateTime::parse_from_str(&dt_str, "%Y-%m-%d %H:%M:%S")?;
+                            let year = no_timezone.format("%Y");
+                            let month = no_timezone.format("%m");
+                            let day = no_timezone.format("%d");
+                            println!("{}-{}-{}", year, month, day);
+                            let current_dir = env::current_dir()?;
+                            println!(
+                                "Entries modified in the last 24 hours in {:?}:",
+                                current_dir
+                            );
+                            let new_path = format!("{}/{}/{}-{}", current_dir.display(), year, month, day);
+                            println!("new_path {}", new_path);
+                            let create_result = create_dir_if_not_exists(&new_path);
+                            match create_result {
+                                Ok(_) => {
+                                    let file_name = file_pwd_path.file_name().unwrap().to_str();
+                                    match file_name {
+                                        Some(file_name) => {
+                                            let new_file_path = format!("{}/{}", new_path, file_name);
+                                            println!("输出新文件路径：{:?}", new_file_path);
+                                            fs::rename(file_pwd_path, new_file_path)?;
+                                        },
+                                        None => {
+                                            println!("输出新文件路径错误");
+                                        }
+                                    }
+                                },
+                                Err(err) => {
+                                    println!("{:?}", err);
+                                }
+                            }
                         },
                         None => eprintln!("DateTimeOriginal tag is missing"),
                     }
@@ -53,6 +86,9 @@ fn get_exif_time_info(dir_path: &str) -> Result<(), Box<dyn std::error::Error>> 
 
 
 fn main() {
-    let resu = get_exif_time_info("./pics");
-    println!("{:?}", resu);
+    
+    for argument in env::args() {
+        get_exif_time_info(&argument);
+    }
+    // println!("{:?}", resu);
 }
